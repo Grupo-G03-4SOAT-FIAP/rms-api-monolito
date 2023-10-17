c2c4a09df09c8be37ad8f93ecb4027e01730b0df
Rafael Silva <todosomtodavoz@gmail.com>
2023-10-16 23:24:10 -0300

    Adicionar extensão da CriaCategoriaDTO para AtualizaCategoriaDTO

--- src/adapters/inbound/rest/v1/presenters/dto/categoria/AtualizaCategoria.dto.ts
+++ src/adapters/inbound/rest/v1/presenters/dto/categoria/AtualizaCategoria.dto.ts
@@ -1,28 +1,3 @@
-import { IsBoolean, IsNotEmpty, IsString, MaxLength } from 'class-validator';
+import { CriaCategoriaDTO } from './CriaCategoria.dto';
 
-export class AtualizaCategoriaDTO {
-  constructor(categoria?: {
-    nome?: string;
-    descricao?: string;
-    ativo?: boolean;
-  }) {
-    this.nome = categoria.nome;
-    this.descricao = categoria.descricao;
-    this.ativo = categoria.ativo;
-  }
-
-  @IsString()
-  @IsNotEmpty({ message: 'Nome da categoria não pode ser vazio' })
-  nome: string;
-
-  @IsString()
-  @IsNotEmpty({ message: 'Descrição da categoria não pode ser vazia' })
-  @MaxLength(1000, {
-    message: 'Descrição não pode ter mais que 1000 caracteres',
-  })
-  descricao: string;
-
-  @IsBoolean()
-  @IsNotEmpty({ message: 'Ativo não pode ser vazio' })
-  ativo: boolean;
-}
+export class AtualizaCategoriaDTO extends CriaCategoriaDTO {}
