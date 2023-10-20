241591b1e5891b87f7b62d484c36d1f37ce53fe1
Rafael Silva <todosomtodavoz@gmail.com>
2023-10-19 22:24:26 -0300

    Adicionar configuração para o class validator e adicionar isoptinal para as propriedades das dtos de atualização

--- src/adapters/inbound/rest/v1/presenters/dto/produto/AtualizaProduto.dto.ts
+++ src/adapters/inbound/rest/v1/presenters/dto/produto/AtualizaProduto.dto.ts
@@ -1,15 +1,54 @@
-import { CriaProdutoDTO } from './CriaProduto.dto';
+import {
+  IsBoolean,
+  IsNotEmpty,
+  IsNumber,
+  IsOptional,
+  IsString,
+  IsUrl,
+  MaxLength,
+  Min,
+} from 'class-validator';
 
-export class AtualizaProdutoDTO extends CriaProdutoDTO {
+export class AtualizaProdutoDTO {
+  @IsString()
+  @IsNotEmpty({ message: 'Nome do produto não pode ser vazia' })
+  @IsOptional()
+  nome: string;
+
+  @IsString()
+  @IsNotEmpty({ message: 'Descrição do produto não pode ser vazia' })
+  @MaxLength(1000, {
+    message: 'Descrição não pode ter mais que 1000 caracteres',
+  })
+  @IsOptional()
+  descricao: string;
+
+  @IsNumber({ maxDecimalPlaces: 2, allowNaN: false, allowInfinity: false })
+  @Min(1, { message: 'O valor precisa ser maior que zero' })
+  @IsOptional()
+  valorUnitario: number;
+
+  @IsUrl({ message: 'URL para imagem inválida' })
+  @IsOptional()
+  imagemUrl: string;
+
+  @IsBoolean()
+  @IsNotEmpty({ message: 'Ativo não pode ser vazio' })
+  @IsOptional()
+  ativo: boolean;
+
+  @IsNumber()
+  @IsOptional()
+  idCategoria: number;
+
   constructor(
     nome?: string,
     descricao?: string,
     valorUnitario?: number,
     imagemUrl?: string,
-    ativo: boolean = true,
-    idCategoria?: string,
+    ativo?: boolean,
+    idCategoria?: number,
   ) {
-    super();
     this.nome = nome;
     this.descricao = descricao;
     this.valorUnitario = valorUnitario;
