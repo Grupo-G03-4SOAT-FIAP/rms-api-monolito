47830b5abd9285576be71537b8f4951708f3a64e
Rafael Silva <todosomtodavoz@gmail.com>
2023-10-21 21:34:18 -0300

    Remover arquivos não utilizados

--- src/adapters/inbound/rest/v1/swagger/categorias/index-categorias-swagger.ts
+++
@@ -1,6 +0,0 @@
-import { OmitType } from '@nestjs/swagger';
-import { CategoriaModel } from '../../../../../outbound/models/categoria.model';
-
-export class IndexCategoriasSwagger extends OmitType(CategoriaModel, [
-  'produtos',
-]) {}
--- src/adapters/inbound/rest/v1/swagger/produtos/index-produtos-swagger.ts
+++
@@ -1,3 +0,0 @@
-import { ProdutoModel } from 'src/adapters/outbound/models/produto.model';
-
-export class IndexProdutosSwagger extends ProdutoModel {}
