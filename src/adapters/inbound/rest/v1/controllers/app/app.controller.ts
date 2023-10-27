ab081eb61017703e6cbc94a2fb61be2acb38a3f7
Rafael Silva <todosomtodavoz@gmail.com>
2023-10-27 08:10:16 -0300

    chore:(31) Configurar responses para app.controller

--- src/adapters/inbound/rest/v1/controllers/app/app.controller.ts
+++ src/adapters/inbound/rest/v1/controllers/app/app.controller.ts
@@ -1,11 +1,23 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppUseCase } from '../../../../../../domain/use_cases/app/app.use_case';
+import { ApiOperation, ApiResponse } from '@nestjs/swagger';
+import { NotFoundSwagger } from '../helpers/swagger/status-codes/not_found.swagger';
 
 @Controller()
 export class AppController {
   constructor(private readonly appUseCase: AppUseCase) {}
 
   @Get()
+  @ApiOperation({ summary: 'RMS API' })
+  @ApiResponse({
+    status: 200,
+    description: 'OK',
+  })
+  @ApiResponse({
+    status: 404,
+    description: 'Página não foi encontrada',
+    type: NotFoundSwagger,
+  })
   healthCheck(): string {
     return this.appUseCase.healthCheck();
   }
