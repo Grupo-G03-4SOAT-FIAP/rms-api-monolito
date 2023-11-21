8da1151c06113f4ca7d8abef0777500eada849ca
Rafael Silva <todosomtodavoz@gmail.com>
2023-11-21 08:08:54 -0300

    feature: validar se a descrição foi passada na criação da entity

--- src/domain/entities/categoria.entity.ts
+++ src/domain/entities/categoria.entity.ts
@@ -2,14 +2,16 @@ import { ToCapitalizeString } from '../value_objects/capitalize_string';
 
 export class CategoriaEntity {
   nome: string;
-  descricao: string;
+  descricao?: string;
   id?: string;
 
-  constructor(nome: string, descricao: string, id?: string) {
+  constructor(nome: string, descricao?: string, id?: string) {
     this.id = id;
     const capitalizedNome = new ToCapitalizeString(nome);
     this.nome = capitalizedNome.input;
-    const capitalizedDescricao = new ToCapitalizeString(descricao);
-    this.descricao = capitalizedDescricao.input || undefined;
+    if (descricao) {
+      const capitalizedDescricao = new ToCapitalizeString(descricao);
+      this.descricao = capitalizedDescricao.input;
+    }
   }
 }
