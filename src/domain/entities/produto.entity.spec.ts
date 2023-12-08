b9365b43db7f0304b39f15df14ab10b982807694
Danilo das Neves Dantas <5115895+dannevesdantas@users.noreply.github.com>
2023-12-08 17:23:32 -0300

    test: Inclui testes para a entidade Produto

---
+++ src/domain/entities/produto.entity.spec.ts
@@ -0,0 +1,24 @@
+import { CategoriaEntity } from './categoria.entity';
+import { ProdutoEntity } from './produto.entity';
+
+describe('Produto Entity', () => {
+  it('Deve ser criado com letras maiusculas o nome e a descrição de uma entidade produto', async () => {
+    const categoriaEntity = new CategoriaEntity(
+      'Lanche',
+      'Lanche x tudo',
+      '0a14aa4e-75e7-405f-8301-81f60646c93d',
+    );
+    
+    const produtoEntity = new ProdutoEntity(
+      'produto X',
+      categoriaEntity,
+      5.0,
+      'http://',
+      'teste produto x',
+      '0a14aa4e-75e7-405f-8301-81f60646c93d',
+    );
+    
+    expect(produtoEntity.nome).toBe('Produto X');
+    expect(produtoEntity.descricao).toBe('Teste Produto X');
+  });
+});
