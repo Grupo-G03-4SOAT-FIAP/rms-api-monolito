e63bd0485c51c8f1b2231e46b6eb1751a336782b
Danilo das Neves Dantas <5115895+dannevesdantas@users.noreply.github.com>
2023-12-16 10:47:27 -0300

    refactor(55): Remove annotations do Swagger das Models do TypeORM (#70)

--- src/adapters/outbound/models/cliente.model.ts
+++ src/adapters/outbound/models/cliente.model.ts
@@ -6,35 +6,27 @@ import {
   PrimaryGeneratedColumn,
   UpdateDateColumn,
 } from 'typeorm';
-import { ApiProperty } from '@nestjs/swagger';
 
 @Entity('clientes')
 export class ClienteModel {
   @PrimaryGeneratedColumn('uuid')
-  @ApiProperty()
   id: string;
 
   @Column({ name: 'nome', length: 100, nullable: false })
-  @ApiProperty()
   nome: string;
 
   @Column({ name: 'email', length: 100, nullable: false })
-  @ApiProperty()
   email: string;
 
   @Column({ name: 'cpf', length: 100, nullable: false })
-  @ApiProperty()
   cpf: string;
 
   @CreateDateColumn({ name: 'criado_em' })
-  @ApiProperty()
   criadoEm: string;
 
   @UpdateDateColumn({ name: 'atualizado_em' })
-  @ApiProperty()
   atualizadoEm: string;
 
   @DeleteDateColumn({ name: 'excluido_em' })
-  @ApiProperty()
   excluidoEm: string;
 }
--- src/adapters/outbound/models/produto.model.ts
+++ src/adapters/outbound/models/produto.model.ts
@@ -9,40 +9,31 @@ import {
   JoinColumn,
 } from 'typeorm';
 import { CategoriaModel } from './categoria.model';
-import { ApiProperty } from '@nestjs/swagger';
 
 @Entity({ name: 'produtos' })
 export class ProdutoModel {
   @PrimaryGeneratedColumn('uuid')
-  @ApiProperty()
   id: string;
 
   @Column({ name: 'nome', length: 100, nullable: false })
-  @ApiProperty()
   nome: string;
 
   @Column({ name: 'descricao', length: 255, nullable: true })
-  @ApiProperty()
   descricao: string;
 
   @Column({ name: 'valor_unitario', nullable: false })
-  @ApiProperty()
   valorUnitario: number;
 
   @Column({ name: 'imagem_url', length: 2048, nullable: false })
-  @ApiProperty()
   imagemUrl: string;
 
   @CreateDateColumn({ name: 'criado_em' })
-  @ApiProperty()
   criadoEm: string;
 
   @UpdateDateColumn({ name: 'atualizado_em' })
-  @ApiProperty()
   atualizadoEm: string;
 
   @DeleteDateColumn({ name: 'excluido_em' })
-  @ApiProperty()
   excluidoEm: string;
 
   @ManyToOne(() => CategoriaModel, (categoria) => categoria.produtos, {
@@ -52,6 +43,5 @@ export class ProdutoModel {
     nullable: false,
   })
   @JoinColumn({ name: 'categoria_id' })
-  @ApiProperty()
   categoria: CategoriaModel;
 }
