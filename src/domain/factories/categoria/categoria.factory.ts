c85370f4621d62aae5388fa6075434b4549d75fc
Rafael Silva <todosomtodavoz@gmail.com>
2024-01-21 10:31:33 -0300

    fix: remover código não utilizado

--- src/domain/factories/categoria/categoria.factory.ts
+++ src/domain/factories/categoria/categoria.factory.ts
@@ -1,7 +1,6 @@
 import { Injectable } from '@nestjs/common';
 import {
   AtualizaCategoriaDTO,
-  CategoriaDTO,
   CriaCategoriaDTO,
 } from 'src/adapters/inbound/rest/v1/presenters/categoria.dto';
 import { CategoriaEntity } from 'src/domain/entities/categoria/categoria.entity';
@@ -9,14 +8,6 @@ import { ICategoriaFactory } from 'src/domain/ports/categoria/categoria.factory.
 
 @Injectable()
 export class CategoriaFactory implements ICategoriaFactory {
-  criarCategoriaDTO(nome: string, descricao: string, id: string): CategoriaDTO {
-    const categoriaDTO = new CategoriaDTO();
-    categoriaDTO.nome = nome;
-    categoriaDTO.descricao = descricao;
-    categoriaDTO.id = id;
-    return categoriaDTO;
-  }
-
   criarEntidadeCategoriaFromCriaCategoriaDTO(
     categoriaDTO: CriaCategoriaDTO,
   ): CategoriaEntity {
