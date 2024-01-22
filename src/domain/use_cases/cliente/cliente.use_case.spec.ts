046c82a4adb16f3c16941e6705296caa12f6aac4
joao0812 <joaoesalmeida@hotmail.com>
2024-01-22 08:26:22 -0300

    fix: resolve o erro do lint da variável declarada nunca utilizada

--- src/domain/use_cases/cliente/cliente.use_case.spec.ts
+++ src/domain/use_cases/cliente/cliente.use_case.spec.ts
@@ -2,6 +2,7 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { ClienteUseCase } from './cliente.use_case';
 import { IClienteRepository } from 'src/domain/ports/cliente/cliente.repository.port';
 import {
+  atualizaClienteDTOMock,
   clienteDTOFactoryMock,
   clienteDTOMock,
   clienteEntityMock,
@@ -10,6 +11,7 @@ import {
   criaClienteDTOMock,
 } from 'src/mocks/cliente.mock';
 import { IClienteDTOFactory } from 'src/domain/ports/cliente/cliente.dto.factory.port';
+import { ClienteNomeUndefinedErro } from 'src/domain/exceptions/cliente.exception';
 
 describe('ClienteUseCase', () => {
   let clienteUseCase: ClienteUseCase;
@@ -45,23 +47,69 @@ describe('ClienteUseCase', () => {
     clienteRepositoryMock.buscarClientePorCPF.mockReturnValue(null);
 
     const result = await clienteUseCase.criarCliente(criaClienteDTOMock);
-    
+
     expect(clienteRepositoryMock.buscarClientePorEmail).toHaveBeenCalledWith(
-        criaClienteDTOMock.email
+      criaClienteDTOMock.email,
     );
     expect(clienteRepositoryMock.buscarClientePorCPF).toHaveBeenCalledWith(
-        criaClienteDTOMock.cpf
+      criaClienteDTOMock.cpf,
     );
     expect(clienteRepositoryMock.criarCliente).toHaveBeenCalledWith(
-        clienteEntityMock,
+      clienteEntityMock,
     );
     expect(clienteDTOFactoryMock.criarClienteDTO).toHaveBeenCalledWith(
-        clienteModelMock,
+      clienteModelMock,
     );
 
     expect(result).toStrictEqual({
-        mensagem: 'Cliente criado com sucesso',
-        body: clienteDTOMock,
+      mensagem: 'Cliente criado com sucesso',
+      body: clienteDTOMock,
     });
+  });
+
+  it('Deve Editar um Cliente com sucesso', async () => {
+    clienteRepositoryMock.buscarClientePorId.mockReturnValue(clienteModelMock);
+    clienteRepositoryMock.buscarClientePorEmail.mockReturnValue(
+      clienteModelMock,
+    );
+
+    clienteRepositoryMock.editarCliente.mockReturnValue(clienteModelMock);
+
+    clienteDTOFactoryMock.criarClienteDTO.mockReturnValue(clienteDTOMock);
+
+    const result = await clienteUseCase.editarCliente(
+      clienteId,
+      atualizaClienteDTOMock,
+    );
+
+    expect(clienteRepositoryMock.buscarClientePorId).toHaveBeenCalledWith(
+      clienteId,
+    );
+    expect(clienteRepositoryMock.buscarClientePorEmail).toHaveBeenCalledWith(
+      atualizaClienteDTOMock.email,
+    );
+    expect(clienteRepositoryMock.editarCliente).toHaveBeenCalledWith(
+      clienteId,
+      atualizaClienteDTOMock
+    )
+    expect(clienteDTOFactoryMock.criarClienteDTO).toHaveBeenCalledWith(
+      clienteModelMock
+    )
+    expect(result).toStrictEqual({
+      mensagem: "Cliente atualizado com sucesso",
+      body: clienteDTOMock
+    })
+  });
+
+  it('Deve dar erro ao Editar nome do Cliente com undefiend', async () => {
+    const cliente = atualizaClienteDTOMock;
+    cliente.nome = undefined;
+    try {
+      await clienteUseCase.editarCliente(clienteId, cliente);
+      fail('A exceção ClienteNomeUndefinedErro deveria aparecer');
+    } catch (error) {
+      expect(error).toBeInstanceOf(ClienteNomeUndefinedErro);
+      expect(error.message).toBe('Informações não preenchidas');
+    }
   });
 });
