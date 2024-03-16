f6628cc9deffc28d9fcd4297e4a0e5266e4b5cbb
Danilo das Neves Dantas <5115895+dannevesdantas@users.noreply.github.com>
2024-03-16 14:53:31 -0300

    Revert "feat: Ignora invariância ao construir ClienteEntity"
    This reverts commit dee98771ec3ec52dc0521406ad60aa503e2dd9c3.

--- src/domain/cliente/entities/cliente.entity.ts
+++ src/domain/cliente/entities/cliente.entity.ts
@@ -8,10 +8,10 @@ export class ClienteEntity {
   private _id?: string;
 
   constructor(nome: string, email: string, cpf?: string, id?: string) {
-    this._id = id;
-    this._nome = nome;
-    this._email = email;
-    this._cpf = cpf;
+    this.id = id;
+    this.nome = nome;
+    this.email = email;
+    this.cpf = cpf;
   }
 
   get nome(): string {
